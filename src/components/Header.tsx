9e76679787efcb569e516ae9d96b66e05a49a638
linnux_x <109190761+linnux-x@users.noreply.github.com>
2026-07-11 16:09:54 +0800

    Change overview icon from wave to globe emoji

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -293,7 +293,7 @@ function Overview() {
   }, [])
   return (
     <section className={"mt-10 flex flex-col md:mt-16 header-timer"}>
-      <p className="text-base font-semibold">👋 {t("overview")}</p>
+      <p className="text-base font-semibold">🌐 {t("overview")}</p>
       <div className="flex items-center gap-1.5">
         <p className="text-sm font-medium opacity-50">{t("whereTheTimeIs")}</p>
         <NumberFlowGroup>
